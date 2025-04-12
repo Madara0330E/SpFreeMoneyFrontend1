3d0017eadba1886b6e9e72ea8d47a058e54f4719
Madara0330E <summoners2011sss@gmail.com>
2025-04-12 17:39:18 +0500

    ваыыыыы
    ваыыыыы

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import { SPWMiniProvider } from "@/components/SPWMiniProvider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
@@ -34,9 +29,7 @@ export default function RootLayout({
           content="frame-ancestors https://spworlds.ru;"
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} antialiased`}>
         <SPWMiniProvider>{children}</SPWMiniProvider>
       </body>
     </html>
